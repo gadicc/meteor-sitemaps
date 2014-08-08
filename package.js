99aa9bf77ca23352b3e65758007dea9922f117c9
Tarang Patel <Tarang@users.noreply.github.com>
2014-08-08 12:32:10 +0200

    Update package.js

--- package.js
+++ package.js
@@ -1,10 +1,12 @@
 Package.describe({
-    summary: "functions to easily output valid sitemaps"
+    summary: "functions to easily output valid sitemaps",
+    version: "0.0.18"
 });
 
 Package.on_use(function (api) {
-  api.use('webapp', 'server');
-	api.use('robots-txt', 'server');
+	if(api.versionsFrom) api.versionsFrom("METEOR-CORE@0.9.0-atm");
+	api.use('webapp', 'server');
+	api.use('gadicohen:robots-txt@0.0.8', 'server');
 	api.add_files('sitemaps.js', 'server');
 	
 	api.export('sitemaps', 'server');
