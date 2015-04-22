cec1c5a2cf0c047acf8ae1f7520ee48209f0808b
Sacha Greif <SachaG@users.noreply.github.com>
2015-04-22 12:43:45 +0900

    Add dependency on check package

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: "gadicohen:sitemaps",
   summary: "functions to easily output valid sitemaps",
-  version: "0.0.20",
+  version: "0.0.21",
   git: 'https://github.com/gadicc/meteor-sitemaps.git'
 });
 
@@ -9,7 +9,7 @@ Package.on_use(function (api) {
   if(api.versionsFrom)
   	api.versionsFrom("METEOR@0.9.0");
 
-  api.use('webapp', 'server');
+  api.use('webapp', 'server', 'check');
   api.use('gadicohen:robots-txt@0.0.8', 'server');
 
   api.add_files('sitemaps.js', 'server');
